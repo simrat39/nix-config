7938559ed2f2f5f4e325f6b39e010712f0c41133
Simrat Grewal <simrats169169@gmail.com>
2025-11-05 22:12:01 -0800

    NixOS Generation 78

--- hosts/modules/gtk.nix
+++ hosts/modules/gtk.nix
@@ -3,24 +3,32 @@
 {
   gtk = {
     enable = true;
-    
+
     theme = {
       name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
-    
+
     iconTheme = {
       name = "Adwaita";
       package = pkgs.adwaita-icon-theme;
     };
-    
+
     # This is important for dark mode preference
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
     };
-    
+
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = true;
+    };
+  };
+
+  # Set dark mode preference via dconf (for modern GNOME/GTK4 apps)
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-theme = "Adwaita-dark";
     };
   };
 }
